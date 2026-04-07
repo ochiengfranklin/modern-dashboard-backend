c379d1d162236155569f83bd62c72518d23a7b3c
Franklin Ochieng <ochiengfranklin264@gmail.com>
2026-04-08 00:05:08 +0300

    implement apminsight with site24*7

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// import('apminsight')
-//     .then(({ default: AgentAPI }) => AgentAPI.config())
-//     .catch(() => console.log('APM not available in this environment'));
+ import('apminsight')
+     .then(({ default: AgentAPI }) => AgentAPI.config())
+     .catch(() => console.log('APM not available in this environment'));
 
 import cors from "cors";
 import express from "express";
-//import { toNodeHandler } from "better-auth/node";
+import { toNodeHandler } from "better-auth/node";
 
 import subjectsRouter from "./routes/subjects.js";
 import usersRouter from "./routes/users.js";
@@ -14,7 +14,7 @@ import statsRouter from "./routes/stats.js";
 import enrollmentsRouter from "./routes/enrollments.js";
 
  import securityMiddleware from "./middleware/security.js";
-// import { auth } from "./lib/auth.js";
+ import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
@@ -38,11 +38,11 @@ app.use(
     })
 );
 
-//app.all("/api/auth/*splat", toNodeHandler(auth));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
-// app.use(securityMiddleware);
+ app.use(securityMiddleware);
 
 app.use("/api/subjects", subjectsRouter);
 app.use("/api/users", usersRouter);
